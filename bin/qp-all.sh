bda0c241fb1a2116d4a0aead34fd0419154549ca
lordyung <jiejin264@qq.com>
2018-08-07 11:52:38 +0800

    rm some command and add zsh_functions.zsh
    Change-Id: I01e1a838f0d0938a1f9dd53d34bf5a9b31f289cb
    Signed-off-by: lordyung <jiejin264@qq.com>

--- bin/qp-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-qshell listbucket distfiles /tmp/.distfiles
-
-for file in `ls *.tar.gz *.tar.xz *.tar.bz2 *.zip`
-do
-    grep $file /tmp/.distfiles > /dev/null || qp ${file}
-done
--- bin/qpb-all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-qshell listbucket book /tmp/.book_list
-
-for file in `ls`
-do
-    grep $file /tmp/.book_list > /dev/null || qp book ${file}
-done
