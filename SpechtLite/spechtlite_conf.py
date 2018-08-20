df919f19e2bca0e2218da160cc3971a0fc10ff3d
lordyung <jiejin264@qq.com>
2018-08-20 17:51:14 +0800

    add SpechtLite Configure
    Change-Id: Ic5ec4064746a7dc558c74a09bf52c34b5eabc9d2
    Signed-off-by: lordyung <jiejin264@qq.com>

---
+++ SpechtLite/spechtlite_conf.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import base64
+import requests
+import re
+import io
+
+def surge_to_specht(domain_type, domain_url):
+    if domain_type == 'DOMAIN-KEYWORD':
+        return '%s\n' % '\.'.join(domain_url.split('.'))
+    elif domain_type == 'DOMAIN-SUFFIX':
+        return '%s$\n' % '\.'.join(domain_url.split('.'))
+    elif domain_type == 'DOMAIN':
+        #todo
+        return '%s$\n' % '\.'.join(domain_url.split('.'))
+
+def get_white_list():
+    print('start to generate whitelist')
+    whitelist_url = 'https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf'
+    data = requests.get(whitelist_url)
+    whitelist_path = './conf/whitelist'
+
+    with open(whitelist_path, 'w') as f:
+        for line in data.iter_lines():
+            f.write(surge_to_specht('DOMAIN-KEYWORD', line.decode('utf-8').split('/')[1]))
+        print('whitelist done!')
+
+def get_gfw_list():
+    print('start to generate gfwlist')
+    gfwlist_url = 'https://raw.githubusercontent.com/gfwlist/gfwlist/master/gfwlist.txt'
+    gfwlist_path = './conf/gfwlist'
+
+    comment_pattern = '^\!|\[|^@@|^\d+\.\d+\.\d+\.\d+'
+    domain_pattern = '([\w\-\_]+\.[\w\.\-\_]+)[\/\*]*'
+
+    data = requests.get(gfwlist_url).text
+    content = base64.b64decode(data)
+
+    with open(gfwlist_path, 'w') as gfwlist_path:
+        for line in io.StringIO(content.decode('utf-8')).readlines():
+            if re.findall(comment_pattern, line):
+                continue
+            else:
+                domain = re.findall(domain_pattern, line)
+                if domain:
+                    if line.startswith('||') or line.startswith('|'):
+                        gfwlist_path.write(surge_to_specht('DOMAIN-SUFFIX', domain[0]))
+                    else:
+                        gfwlist_path.write(surge_to_specht('DOMAIN-KEYWORD', domain[0]))
+    print('gfwlist done!')
+
+def get_reject_list():
+    print('start to get rejectlist')
+    rejectlist_url = 'https://raw.githubusercontent.com/BurpSuite/CloudGate-RuleList/master/Rule/REJECT'
+    rejectlist_path = './conf/rejectlist'
+    data = requests.get(rejectlist_url)
+    with open(rejectlist_path, 'w') as f:
+        for line in data.iter_lines():
+            domain = line.decode('utf-8').split(',')
+            if len(domain) > 1:
+                f.write(surge_to_specht(domain[0], domain[1]))
+    print('rejectlist done!')
+
+if __name__ == '__main__':
+    get_gfw_list()
+    get_white_list()
+    get_reject_list()
